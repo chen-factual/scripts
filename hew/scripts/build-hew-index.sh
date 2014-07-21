3a82e799cabbcd430b1e3763be8f3fae5c4b4f37
Chen Guo <chen@factual.com>
2014-07-21 11:27:37 -0700

    HEW running scripts

--- hew/scripts/build-hew-index.sh
+++ hew/scripts/build-hew-index.sh
@@ -7,4 +7,4 @@ hadoop jar ./hew-hdfs-index.jar mapreduce.HdfsIndex \
     dataset_id=Wu8mjQ \
     base_path=/apps/extract/poi/ItalyScarecrow/output \
     run_name=20140523_181617_PDT_italy_pod_batchsummary_bm02-140523-181617-pehewi-633-3.17 \
-    hew_step=030_grouped_deduped_entities
+    hew_step=032_cluster_refinement
